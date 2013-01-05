4322919b44cb2cab67b51f07f44ebe73ad526ad2
Olivier Penhoat <openhoat@gmail.com>
2013-01-05 13:18:50 +0100

    debug

--- controllers/main/index.js
+++ controllers/main/index.js
@@ -1,13 +1,10 @@
 var wbp = require('wbpjs')
-  , config = wbp.config
-  , webapps = require(wbp.findAppFile('models','webapps.js'))
-  , mvcPlugin = wbp.findPlugin('wbpjs-mvc')
-  , viewsPlugin = mvcPlugin.viewsPlugin;
+  , webapps = require(wbp.findAppFile('models','webapps.js'));
 
 module.exports = {
   index:function (req, res) {
-    viewsPlugin.render(res, function (type) {
-      var view = viewsPlugin.getWebView(req, 'main/index', type);
+    wbp.render(res, function (type) {
+      var view = wbp.getWebView(req, 'main/index', type);
       res.render(view, { webapps: webapps });
     });
   }
